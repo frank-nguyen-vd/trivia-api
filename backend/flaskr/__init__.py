11068c3aced14aa624a69c0d9f91d0d5fdd15a6c
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-04-24 17:13:53 +0800

    🎨 prettify backend code

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -31,16 +31,16 @@ def create_app(test_config=None):
             "Access-Control-Allow-Methods", "GET,PATCH,POST,DELETE,OPTIONS"
         )
         return response
-    """
-    @TODO: 
-    Create an endpoint to handle GET requests 
-    for all available categories.
-    """
 
     @app.route("/api/v1")
     def index():
         return jsonify({"success": True, "message": "Welcome to Trivia-API"})
 
+    """
+    @TODO: 
+    Create an endpoint to handle GET requests 
+    for all available categories.
+    """
     @app.route("/api/v1/categories")
     def find_categories():
         return jsonify(
@@ -57,6 +57,18 @@ def create_app(test_config=None):
             }
         )
 
+    """
+    @TODO: 
+    Create an endpoint to handle GET requests for questions, 
+    including pagination (every 10 questions). 
+    This endpoint should return a list of questions, 
+    number of total questions, current category, categories. 
+
+    TEST: At this point, when you start the application
+    you should see questions and categories generated,
+    ten questions per page and pagination at the bottom of the screen for three pages.
+    Clicking on the page numbers should update the questions. 
+    """
     @app.route("/api/v1/questions")
     def find_questions():
         return jsonify(
@@ -90,19 +102,6 @@ def create_app(test_config=None):
                 "current_category": "Art",
             }
         )
-
-    """
-    @TODO: 
-    Create an endpoint to handle GET requests for questions, 
-    including pagination (every 10 questions). 
-    This endpoint should return a list of questions, 
-    number of total questions, current category, categories. 
-
-    TEST: At this point, when you start the application
-    you should see questions and categories generated,
-    ten questions per page and pagination at the bottom of the screen for three pages.
-    Clicking on the page numbers should update the questions. 
-    """
 
     """
     @TODO: 
