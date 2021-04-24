8ded84510c0f0a5cbc4f11a0d241a8a9fccf6ad4
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-04-24 09:56:11 +0800

    create a bash script to install all required dependencies for back-end
    including linux packages and python packages

---
+++ backend/install-dependencies.sh
@@ -0,0 +1,7 @@
+sudo apt install python3-virtualenv
+sudo apt-get install postgresql
+sudo apt install python3-dev
+sudo apt-get install libpq-dev
+
+source env/bin/activate
+pip install -r requirements.txt
