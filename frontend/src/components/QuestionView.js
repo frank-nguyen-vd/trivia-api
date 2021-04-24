6e213398486adfd64fd9cbc514885a49cbc01182
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-04-24 16:46:55 +0800

    🏗 change the base api from / to /api/v1/ in frontend

--- frontend/src/components/QuestionView.js
+++ frontend/src/components/QuestionView.js
@@ -23,7 +23,7 @@ class QuestionView extends Component {
 
   getQuestions = () => {
     $.ajax({
-      url: `/questions?page=${this.state.page}`, //TODO: update request URL
+      url: `/api/v1/questions?page=${this.state.page}`, //TODO: update request URL
       type: "GET",
       success: (result) => {
         this.setState({
@@ -60,7 +60,7 @@ class QuestionView extends Component {
 
   getByCategory= (id) => {
     $.ajax({
-      url: `/categories/${id}/questions`, //TODO: update request URL
+      url: `/api/v1/categories/${id}/questions`, //TODO: update request URL
       type: "GET",
       success: (result) => {
         this.setState({
